b26ce9ace21a939426fc730bc52b72ddaf433bdd
ritochkachist <131471769+ritochkachist@users.noreply.github.com>
2023-04-24 22:49:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# Margarita Chistiakova
+# 11/15/2022
+# Additional Project 11
+
+print("Let's learn more about acronyms used in English!")
+
+laughter = {
+  'LOL': 'Laugh Out Loud',
+  'LMAO': 'Laughing My A** Off', 
+}
+
+actions = {
+  'TTYL': 'Talk To You Later',
+  'CU': 'See You',
+  'DIY': 'Do It Yourself',
+  'CTN': "Can't Talk Now",
+  'W8': 'Wait'
+}
+
+phrases = {
+  'GJ': 'Good Job',
+  'GM': 'Good Morning',
+  'HMU': 'Hit Me Up',
+  'PLS': 'Please',
+  'GTG': 'Got To Go',
+  'BTW': 'By The Way'
+}
+
+phrases['ILY'] = 'I Love You'
+phrases['JK'] = 'Just Kidding'
+phrases['GR8'] = 'Great'
+
+print('-' * 20)
+
+print("LOL means: ", laughter['LOL'])
+print("DIY means: ", actions['DIY'])
+print("GR8 means: ", phrases['GR8'])
+print("TTYL means: ", actions['TTYL'])
+print("HMU means: ", phrases['HMU'])
+print("BTW means: ", phrases['BTW'])
+print("PLS means: ", phrases['PLS'])
+print("JK means: ", phrases['JK'])
+
+
